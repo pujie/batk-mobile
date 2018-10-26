84b639c176c9b47b95eaebc2eb2b3eb0ee4a21dd
pujie <puji@gmail.com>
2018-10-26 16:36:41 +0700

    use production server

--- src/providers/appvar/appvar.ts
+++ src/providers/appvar/appvar.ts
@@ -9,9 +9,9 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class AppvarProvider {
-  host = "http://localhost"
+  host = "http://belanjaatk.co.id"
   port = "1900"
-  server = this.host+":"+this.port
+  server = this.host
   constructor(public http: HttpClient) {
     console.log('Hello AppvarProvider Provider');
   }
